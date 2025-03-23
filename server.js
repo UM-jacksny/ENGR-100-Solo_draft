c870829a1f9f5c4105bc961d339d9ecb1d4c920a
UM-jacksny <jacksny@umich.edu>
2025-03-23 18:19:06 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'your-email@gmail.com',
+    pass: 'your-password'
+  }
+});
+
+app.post('/send-email', (req, res) => {
+  const { email } = req.body;
+
+  const mailOptions = {
+    from: 'your-email@gmail.com',
+    to: email,
+    subject: 'Assignment Not Submitted on Time',
+    text: 'Your friend did not submit their assignment on time. Maybe give them a little nudge!'
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error(error);
+      return res.status(500).send('Error sending email');
+    }
+    res.status(200).send('Email sent: ' + info.response);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
